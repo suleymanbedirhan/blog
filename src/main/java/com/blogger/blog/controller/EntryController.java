ddcb26a7fae72441deaf9ebe4502415d17c2e4cf
silemanbed <suleymanbedirhanoglu@gmail.com>
2018-11-04 16:34:56 +0300

    sonar fix

--- src/main/java/com/blogger/blog/controller/EntryController.java
+++ src/main/java/com/blogger/blog/controller/EntryController.java
@@ -1,15 +1,16 @@
 package com.blogger.blog.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blogger.blog.model.Entry;
@@ -23,21 +24,20 @@ public class EntryController {
 	@Autowired
 	EntryService entryService;
 	
-	@RequestMapping(value="/list", method=RequestMethod.GET)
+	@GetMapping(value="/list")
 	public ResponseEntity<List<Entry>> findAll() {
-		List<Entry> entries = new ArrayList<Entry>();
-		entries = entryService.findAll();
+		List<Entry> entries = entryService.findAll();
 		return new ResponseEntity<List<Entry>>(entries, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value="/add", method=RequestMethod.POST)
+	@PostMapping(value="/add")
 	public ResponseEntity<Entry> saveEntry(@RequestBody EntryVO vo) {
 		Entry entry = entryService.addEntry(vo);
 		
 		return new ResponseEntity<Entry>(entry, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value="/{entryId}", method=RequestMethod.GET)
+	@GetMapping(value="/{entryId}")
 	public ResponseEntity<Entry> findEntryById(@PathVariable("entryId") Long id) {
 		Entry entry = entryService.findEntryById(id);
 		if(entry == null) {
@@ -46,14 +46,14 @@ public class EntryController {
 		return new ResponseEntity<Entry>(entry, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value="/{entryId}", method=RequestMethod.POST)
+	@DeleteMapping(value="/{entryId}")
 	public ResponseEntity<Entry> deleteEntryById(@PathVariable("entryId") Long id) {
 		entryService.deleteEntry(id);
 		return new ResponseEntity<Entry>(HttpStatus.OK);
 	}
 	
 	
-	@RequestMapping(value="/update", method=RequestMethod.POST)
+	@PostMapping(value="/update")
 	public ResponseEntity<Entry> updateEntry(@RequestBody EntryVO entryVo) {
 		
 		entryService.updateEntry(entryVo);
